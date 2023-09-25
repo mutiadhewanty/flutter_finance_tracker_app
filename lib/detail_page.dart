207b18a281bd891b01d9713a6b2d83a9a7bc4d62
mutiadhewanty <mutiadhewanty@gmail.com>
2023-09-25 15:56:01 +0700

    details

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -15,6 +15,55 @@ class _DetailPageState extends State<DetailPage> {
         title: const Text('Detail Cash Flow'),
         backgroundColor: const Color(0xFF3E616B),
       ),
+      body: ListView.builder(itemBuilder: ((context, index) {
+        return Card(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      '[+] Rp 250.000',
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF2B4856)),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    const Text(
+                      'Dapat bayaran panitia sertifikasi',
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF2B4856)),
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Text(
+                      '25-09-2023',
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600,
+                          color: const Color(0xFF2B4856).withOpacity(0.5)),
+                    ),
+                  ],
+                ),
+              ),
+              Icon(
+                Icons.arrow_back_rounded,
+                size: 45,
+                color: Colors.green.shade700,
+              )
+            ],
+          ),
+        );
+      })),
     );
   }
 }
