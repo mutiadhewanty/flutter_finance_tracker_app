0a079b5bd55f5502d2eb4b65b485b066f8d5e45f
mutiadhewanty <mutiadhewanty@gmail.com>
2023-09-29 05:05:50 +0700

    user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,15 +1,13 @@
 class UserModel {
-  const UserModel(
-    {
-      this.id,
-      required this.username,
-      required this.password
-    }
-  );
+  const UserModel({this.id, required this.username, required this.password});
 
   final int? id;
   final String username;
   final String password;
 
-  factory
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+      id: json['id'], username: json['username'], password: json['password']);
+
+  Map<String, dynamic> toJson() =>
+      {'id': id, 'username': username, 'password': password};
 }
--- lib/services/database_helper.dart
+++ lib/services/database_helper.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_finance_tracker_app/models/finance_model.dart';
+import 'package:flutter_finance_tracker_app/models/user_model.dart';
 // ignore: depend_on_referenced_packages
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
@@ -9,7 +10,16 @@ class DatabaseHelper {
 
   static Future<Database> _getDB() async {
     return openDatabase(join(await getDatabasesPath(), _dbName),
-        onCreate: (db, version) async => await db.execute('''
+        onCreate: (db, version) async {
+      await db.execute('''
+        CREATE TABLE User(
+          id INTEGER PRIMARY KEY,
+          username TEXT NOT NULL,
+          password TEXT NOT NULL
+        )
+      ''');
+
+      await db.execute('''
   CREATE TABLE Finance(
     id INTEGER PRIMARY KEY,
     date TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -17,7 +27,27 @@ class DatabaseHelper {
     keterangan TEXT NOT NULL,
     kategori TEXT NOT NULL
   )
-'''), version: _version);
+''');
+    }, version: _version);
+  }
+
+  static Future<int> addUser(UserModel user) async {
+    final db = await _getDB();
+    return await db.insert("User", user.toJson(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  static Future<UserModel?> loginUser(String username, String password) async {
+    final db = await _getDB();
+    final List<Map<String, dynamic>> maps = await db.query(
+      "User",
+      where: "username = ? AND password = ?",
+      whereArgs: [username, password],
+    );
+    if (maps.isEmpty) {
+      return null;
+    }
+    return UserModel.fromJson(maps[0]);
   }
 
   static Future<int> addFinance(Finance finance) async {
